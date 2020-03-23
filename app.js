9b2eb2c66ae1271513a95b384bae88098414843d
Bruno Rangel <brunojp.rangel@gmail.com>
2020-03-23 00:43:04 -0300

    Last version

--- app.js
+++ app.js
@@ -5,26 +5,33 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const apiURL = `https://api.lyrics.ovh`
 
-const getMoreSongs = async url => {
-    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
-    const data = await response.json()
+const fetchData = async url => {
+    const response = await fetch(url)
+    return await response.json()
+}
 
+const getMoreSongs = async url => {
+    const data = await fetchData(`https://cors-anywhere.herokuapp.com/${url}`)
     insertSongsIntoPage(data)
 }
 
-const insertSongsIntoPage = songsInfo => {
-    songsContainer.innerHTML = songsInfo.data.map(song => `
+const insertNextAndPrevButtons = ({ prev, next }) => {
+    prevAndNextContainer.innerHTML = `
+        ${prev ? `<button class="btn" onClick="getMoreSongs('${prev}')">Anteriores</button>` : ''}
+        ${next ? `<button class="btn" onClick="getMoreSongs('${next}')">Próximas</button>` : ''}
+        `
+}
+
+const insertSongsIntoPage = ({ data, prev, next}) => {
+    songsContainer.innerHTML = data.map(({ artist: { name }, title}) => `
         <li class="song">
-            <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
-            <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
+            <span class="song-artist"><strong>${name}</strong> - ${title}</span>
+            <button class="btn" data-artist="${name}" data-song-title="${title}">Ver letra</button>
         </li>
     `).join('')
 
-    if (songsInfo.prev || songsInfo.next) {
-        prevAndNextContainer.innerHTML = `
-        ${songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : ''}
-        ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : ''}
-        `
+    if (prev || next) {
+        insertNextAndPrevButtons({ prev, next })
         return
     }
 
@@ -32,16 +39,16 @@ const insertSongsIntoPage = songsInfo => {
 }
 
 const fetchSongs = async term => {
-    const response = await fetch(`${apiURL}/suggest/${term}`)
-    const data = await response.json()
-
+    const data = await fetchData(`${apiURL}/suggest/${term}`)
     insertSongsIntoPage(data)
 }
 
-form.addEventListener('submit', event => {
+const handleFormSubmit = event => {
     event.preventDefault()
 
     const searchTerm = searchInput.value.trim()
+    searchInput.value = ''
+    searchInput.focus()
     
     if (!searchTerm) {
         songsContainer.innerHTML = `<li class="warning-message">Por favor, digite um termo válido</li>`
@@ -49,13 +56,11 @@ form.addEventListener('submit', event => {
     }
 
     fetchSongs(searchTerm)
-})
+}
 
-const fetchLyrics = async (artist, songTitle) => {
-    const response = await fetch(`${apiURL}/v1/${artist}/${songTitle}`)
-    const data = await response.json()
-    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
+form.addEventListener('submit', handleFormSubmit)
 
+const insertLyricsIntoPage = ({ lyrics, artist, songTitle }) => {
     songsContainer.innerHTML = `
         <li class="lyrics-container">
             <h2><strong>${songTitle}</strong> - ${artist}</h2>
@@ -64,7 +69,13 @@ const fetchLyrics = async (artist, songTitle) => {
     `
 }
 
-songsContainer.addEventListener('click', event => {
+const fetchLyrics = async (artist, songTitle) => {
+    const data = await fetchData(`${apiURL}/v1/${artist}/${songTitle}`)
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
+    insertLyricsIntoPage({ lyrics, artist, songTitle }) 
+}
+
+const handleSongsContainerClick = event => {
     const clickedElement = event.target
 
     if(clickedElement.tagName === 'BUTTON') {
@@ -76,4 +87,7 @@ songsContainer.addEventListener('click', event => {
         fetchLyrics(artist, songTitle)
     }
     
-})
+}
+
+
+songsContainer.addEventListener('click', handleSongsContainerClick)
